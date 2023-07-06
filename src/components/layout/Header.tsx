612b8eec859fcd8c6491dc77b71dbb2cacc02e36
Eunseo Na <nes020781@gmail.com>
2023-07-06 22:05:54 +0900

    design: button hover

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 /**
  * 헤더 (상단바)
  */
@@ -7,14 +7,22 @@ import React from "react";
 export function Header() {
   return (
     <div className="flex flex-col px-5 pt-5">
-      <div className="flex items-center">
+      <div className="flex items-center ">
         <span className="text-[#ff0099] text-2xl font-sans font-bold">
           SHOTPING
         </span>
         <div className="flex pl-10 space-x-10">
-          <div className="hover:underline-offset-4 text-lg">상품인식</div>
-          <div className="hover:underline-offset-4 text-lg">상품리스트 </div>
-          <div className="hover:underline-offset-4 text-lg">결제 </div>
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="hover:font-semibold text-lg">상품인식</div>
+          </motion.button>
+
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="hover:font-semibold text-lg">상품리스트</div>
+          </motion.button>
+
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="hover:font-semibold text-lg">결제</div>
+          </motion.button>
         </div>
       </div>
       <div className="flex px-4 py-2 w-full border-b-2" />
