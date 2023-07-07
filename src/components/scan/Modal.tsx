a28ee17dcff9f1dccc764bdafb5fc29f00a8635f
Ahnnakyung <lr1oqa@naver.com>
2023-07-07 23:43:00 +0900

    design:인식 페이지 구현중

---
+++ src/components/scan/Modal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+function Modal({ setModalOpen }: { setModalOpen: () => void }) {
+  return <div className="modal">모달창입니다.</div>;
+}
+export default Modal;
--- src/pages/ScanPage.tsx
+++ src/pages/ScanPage.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Modal from "../src/components/scan/Modal";
 
-export default function ScanPage() {
+function ScanPage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const showModal = () => {
+    setModalOpen(true);
+  };
   return (
-    <div>
-      <div>scanpage</div>
+    <div className="align flex flex-col items-center justify-center min-h-screen">
+      <div className="camera flex justify-center mt-0.5">
+        <button onClick={showModal}>
+          <img
+            src="https://i.postimg.cc/HLC973gD/2023-07-06-224526.png"
+            className="image w-20 h-auto"
+            alt="Camera"
+          />
+        </button>
+        {modalOpen && <Modal setModalOpen={setModalOpen} />}
+      </div>
+      <div className="text pt-2 text-center">
+        <div>상품을 인식해 주세요!</div>
+      </div>
     </div>
   );
 }
+
+export default ScanPage;
