e7dde8d371ae9bac8126d650fca2da286a9913c6
Eunseo Na <nes020781@gmail.com>
2023-07-07 23:37:42 +0900

    design: 리스트 퍼블리싱 진행 중

---
+++ src/components/list/ProductBox.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductBox() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { SearchBox } from "../components/list/SearchBox";
 
 export default function ListPage() {
   return (
-    <div>
-      <div>listpage</div>
+    <div className="flex flex-col border mx-32 my-20">
+      <SearchBox />
+      <div className="my-20 border">hello</div>
     </div>
   );
 }
