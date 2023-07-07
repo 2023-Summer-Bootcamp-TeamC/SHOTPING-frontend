1ef85ef3485997869014eb071a71056b1f4f39aa
Eunseo Na <nes020781@gmail.com>
2023-07-08 02:54:04 +0900

    design: 웹캠으로 촬영하는 컴포넌트 구현 완료

--- src/pages/ScanPage.tsx
+++ src/pages/ScanPage.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import ScanStart from "../components/scan/ScanStart";
+import Scanning from "../components/scan/Scanning";
 
 function ScanPage() {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const showModal = () => {
-    setModalOpen(true);
-  };
   return (
     <div className="align flex flex-col items-center justify-center min-h-screen">
-      <ScanStart />
+      {/* <ScanStart /> */}
+      <Scanning />
     </div>
   );
 }
