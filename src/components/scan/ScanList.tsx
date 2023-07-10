5e272401febccbe0f6be7bb1d72124adb1dfbb53
Eunseo Na <nes020781@gmail.com>
2023-07-10 21:14:26 +0900

    design: ScanPage의 ScanList 이미지가 가운데 정렬되도록 조정

--- src/components/scan/ScanList.tsx
+++ src/components/scan/ScanList.tsx
@@ -4,7 +4,12 @@ export default function ScanList() {
   return (
     <div className="flex flex-col">
       <div className=" flex items-center space-x-9 place-content-center p-4 pr-1">
-        <div className="w-20 h-24 bg-blue-300"></div>
+        <div className="relative w-20 h-24 bg-white flex justify-center items-center">
+          <img
+            src="https://i.postimg.cc/HLC973gD/2023-07-06-224526.png"
+            className="absolute "
+          />
+        </div>
         <span className="truncate text-lg w-2/4">
           지금까지 이런맛은 없었다 이건 갈비인가 치킨인가
         </span>
