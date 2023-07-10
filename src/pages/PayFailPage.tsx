d85e8ac560d3bdc8c730b0b81b4bdca54cebdcea
HYEJIN KIM <20_khj@naver.com>
2023-07-10 17:50:10 +0900

    design: 결제완료 및 모달, 결제실패 퍼블리싱 완료

--- src/pages/PayFailPage.tsx
+++ src/pages/PayFailPage.tsx
@@ -1,9 +1,63 @@
 import React from "react";
+import PayButton from "../components/Pay/PayButton";
+import PayText from "../components/Pay/PayText";
+import payfail_image from "../components/images/payfail_image.png";
+import { useNavigate } from "react-router-dom";
 
-export default function PayFailPage() {
+const flexColumnCenterStyle = () => {
+  return "flex flex-col items-center justify-center h-screen";
+};
+
+const PayFailPage: React.FC = () => {
+  const navigate = useNavigate(); // Get the navigate function from React Router
+
+  const handleRetry = () => {
+    // Logic for retrying payment
+    console.log("다시 시도하기 button clicked");
+  };
+
+  const handleGoBack = () => {
+    navigate("/main"); // Navigate to the "/main" route when the button is clicked
+  };
+
   return (
-    <div>
-      <div>payfailpage</div>
+    <div className={flexColumnCenterStyle()}>
+      <img
+        src={payfail_image}
+        alt="Payment"
+        style={{
+          width: "7.5rem",
+          height: "7.5rem",
+          marginTop: "8.75rem",
+          marginBottom: "1.875rem",
+        }}
+      />
+      <PayText name="결제가 완료되지 않았습니다." />
+
+      <div
+        style={{
+          marginRight: "35rem",
+          marginTop: "6.25rem",
+          marginBottom: "0.625rem",
+        }}
+      ></div>
+
+      <PayButton
+        variant="continue"
+        name="다시 시도하기"
+        onClick={() => {
+          navigate("/buy");
+        }}
+      />
+      <PayButton
+        variant="detail"
+        name="메인으로 돌아가기"
+        onClick={() => {
+          navigate("/");
+        }}
+      />
     </div>
   );
-}
+};
+
+export default PayFailPage;
