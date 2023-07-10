e9c7ffda422a378337153c93cb12549639b9c056
Ahnnakyung <lr1oqa@naver.com>
2023-07-10 23:00:33 +0900

    fix: 충돌 수정

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { SearchBox } from "../components/list/SearchBox";
 import ProductBox from "../components/list/ProductBox";
-<<<<<<< HEAD
 import ProductKey from "../components/list/ProductKey";
 import ProductList from "../components/list/ProductList";
 
@@ -13,32 +12,6 @@ export default function ListPage() {
       </div>
       <div className="h-3/4 overflow-y-auto scrollbar-hide">
         <ProductKey ProductList={ProductList} />
-=======
-
-export default function ListPage() {
-  return (
-    <div className="w-full px-20 mt-12 ml-4">
-      <div>
-        <SearchBox />
-      </div>
-      <div className="grid grid-cols-4 gap-y-10 mt-14 mb-20">
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
->>>>>>> #8
       </div>
     </div>
   );
