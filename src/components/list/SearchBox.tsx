d8b3532bdf495441bbf6be30687a95b1ca154b07
Eunseo Na <nes020781@gmail.com>
2023-07-13 17:16:39 +0900

    feat: searchbox에서 검색 시 searchResult로 라우팅 되도록 구현

--- src/components/list/SearchBox.tsx
+++ src/components/list/SearchBox.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, ChangeEvent, KeyboardEvent } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export function SearchBox() {
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+
+  const searchOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+  const searchClick = () => {
+    if (searchInput === "") {
+      alert("검색어를 입력해 주세요.");
+    }
+    navigate("/searchresult");
+  };
+
+  const enterSearch = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (searchInput === "") {
+        alert("검색어를 입력해 주세요.");
+      }
+
+      navigate("/searchresult");
+    }
+  };
+
   return (
     <div className="flex items-center text-center">
       <input
         type="text"
         placeholder="상품명을 입력하세요"
+        onChange={searchOnChange}
+        onKeyPress={enterSearch}
         className="w-80 h-14 text-xl px-2 py-2 border-b border-gray-300 focus:outline-none focus:none"
       />
-      <button type="button" className="mr-2 mt-1">
+      <button type="button" className="mr-2 mt-1" onClick={searchClick}>
         <FaSearch className="w-7 h-7 text-gray-300" />
       </button>
     </div>
