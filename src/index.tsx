f4fec4b7ba0c80fd2b6c7a2e83b5177b8a4c7994
Eunseo Na <nes020781@gmail.com>
2023-07-24 17:16:51 +0900

    feat: index setting

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "./store/store";
+
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.Fragment>
+        <App />
+      </React.Fragment>
+    </PersistGate>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
