aa0ba8b44b626e540b43e0e5fe4c1bec22c63950
Ahnnakyung <lr1oqa@naver.com>
2023-07-26 22:49:08 +0900

    design: 메인모달 중간점검

--- src/components/main/MainModal.tsx
+++ src/components/main/MainModal.tsx
@@ -13,21 +13,21 @@ export default function Modal({ setIsModalOpen }: MainModalProps) {
 
   return (
     <div
-      className="md modal flex justify-center items-center fixed z-50 w-full h-full top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50"
+      className="xl modal flex justify-center items-center fixed z-50 w-full h-full top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50"
       style={{ cursor: "default" }}
       onClick={setIsModalOpen}
     >
-      <div className="w-[35rem] md:modalContent md:w-[60rem] md:h-[40rem] md:mt-16 bg-white rounded-xl flex flex-col justify-center items-center">
-        <div className="mt-12 mb-[1rem] text-2xl font-semibold 2xl:mb-2 md:mt-4 transition-all duration-700">
+      <div className="w-[35rem] xl:w-[60rem] xl:h-[40rem] xl:mt-16 bg-white rounded-xl flex flex-col justify-center items-center">
+        <div className="mt-12 mb-[1rem] text-2xl font-semibold 2xl:mb-2 xl:mt-4 transition-all duration-700">
           지금 인기있는 상품들이에요!
         </div>
         <div className="flex">
-          <div className="md:chartContainer">
-            <div className="hidden md:chart md:w-[27rem] md:h-[32rem] md:mr-4 md:flex justify-center items-center h-full">
+          <div className="xl:chartContainer">
+            <div className="hidden xl:chart xl:w-[27rem] xl:h-[32rem] xl:mr-4 xl:flex justify-center items-center h-full">
               <Chart />
             </div>
           </div>
-          <div className="mobile mb-4 md:ranking md:w-[27rem] md:h-[32rem] md:ml-4">
+          <div className="mobile mb-4 xl:ranking xl:w-[27rem] xl:h-[32rem] xl:ml-4">
             <Ranking />
           </div>
         </div>
