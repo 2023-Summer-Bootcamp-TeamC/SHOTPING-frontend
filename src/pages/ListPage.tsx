47daf6774f654a921f28ae1b812f5a80846f1556
HYEJIN KIM <20_khj@naver.com>
2023-07-30 17:56:28 +0900

    design: 리스트 페이지 밑에 하얀색 부분 안보이게 수정 완료

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -6,10 +6,10 @@ import ProductKey from "../components/list/ProductKey";
 export default function ListPage() {
   return (
     <div className="w-full flex flex-col items-center justify-center h-full">
-      <div className="pb-6 w-3/4">
+      <div className="pb-6 pt-20 w-3/4">
         <SearchBox />
       </div>
-      <div className="h-3/4 w-3/4 overflow-y-auto scrollbar-hide">
+      <div className="h-full w-3/4 overflow-y-auto scrollbar-hide">
         <ProductKey />
       </div>
     </div>
